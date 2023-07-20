1f4ca4519276958c36a6f88cf821cf7ce93b7364
shreyaramakrishnan <sr66@uw.edu>
2023-07-20 16:28:51 -0700

    fix naming

---
+++ basics-and-printing/StarFigures1.java
@@ -0,0 +1,15 @@
+/*
+Using println statements, output the following figure to console. 
+
+****
+***
+** 
+*
+
+*/ 
+public class StarFigures1 {
+    public static void main(String[] args) {
+        // Code goes here.  
+
+    }
+}
