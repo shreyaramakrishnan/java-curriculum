e98c46c62fcfaa0e628f0bd800b01589ad84e2b4
Srithika Barakam <srithikabarakam@Srithikas-MBP.attlocal.net>
2023-07-23 18:37:58 -0700

    720

--- basics-and-printing/StarFigures1.java
+++ basics-and-printing/StarFigures1.java
@@ -1,15 +1,40 @@
 /*
 Using println statements, output the following figure to console. 
+*****
+*****
+ * *
+  *
+ * *
 
-****
-***
-** 
-*
+  *
+  *
+  *
+*****
+*****
+ * *
+  *
+ * *
 
-*/ 
+ */   
+
+
 public class StarFigures1 {
     public static void main(String[] args) {
         // Code goes here.  
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println(" * * ");
+        System.out.println("  *");
+        System.out.println(" * * ");
+        System.out.println();
+        System.out.println("  *");
+        System.out.println("  *");
+        System.out.println( "  * ");
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println(" * * ");
+        System.out.println("  *");
+        System.out.println(" * * ");
 
     }
 }
