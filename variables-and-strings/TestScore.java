00429ee1852003e66307bbc00d93c7b2c1404ba6
Srithika Barakam <srithikabarakam@Srithikas-MBP.attlocal.net>
2023-08-20 16:17:26 -0700

    hw

--- variables-and-strings/TestScore.java
+++ variables-and-strings/TestScore.java
@@ -3,7 +3,7 @@ public class TestScore {
         int mathScore = 490;
         int readingScore = 710;
         int writingScore = 350;
-        double totalScore = ((350+710+490)* 1/3 );
+        double totalScore = ((mathScore + readingScore + writingScore)* 1/3 );
         System.out.println(totalScore);
     }
 }
